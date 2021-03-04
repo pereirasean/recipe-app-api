b27fdbc93a57a8bf190f3b2bc5c4fa55f52d12a4
Sean Pereira <pereirasean1811@gmail.com>
2021-03-04 11:05:15 +0000

    added user registration and superuser config

--- app/app/settings.py
+++ app/app/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -117,4 +118,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+
+
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'core.User'
