c21ee20faf85b50d0d157eb267c451e3f3c86e5f
Sean Pereira <pereirasean1811@gmail.com>
2021-03-05 09:29:55 +0000

    configured postgres db

--- app/core/admin.py
+++ app/core/admin.py
@@ -8,26 +8,26 @@ class UserAdmin(BaseUserAdmin):
     ordering = ['id']
     list_display = ['email', 'name']
     fieldsets = (
-    (None, {'fields': ('email', 'password')}),
-    (_('Personal Info'), {'fields': ('name',)}),
-    (
-        _('Permissions'),
-        {
-            'fields': (
-                'is_active',
-                'is_staff',
-                'is_superuser',
-            )
-        }
-    ),
-    (_('Important dates'), {'fields': ('last_login',)}),
-)
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal Info'), {'fields': ('name',)}),
+        (
+            _('Permissions'),
+            {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                )
+            }
+        ),
+        (_('Important dates'), {'fields': ('last_login',)}),
+    )
     add_fieldsets = (
-    (None, {
-        'classes': ('wide',),
-        'fields': ('email', 'password1', 'password2')
-    }),
-)
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2')
+        }),
+    )
 
 
 admin.site.register(models.User, UserAdmin)
